3e119fec36c6ce5e4f2a591d07285968a9b2657d
Masashi Okada <okadaalgorithm@gmail.com>
2018-05-01 21:05:51 +0000

    version 1.2

--- inst/doc/CRPClustering-vignette.R
+++
@@ -1,6 +0,0 @@
-## ----fig.width=13.0,fig.height=4.0,fig.align="center",fig.cap="CRP clustering result",echo=FALSE----
-library(png)
-library(grid)
-img <- readPNG("./image.png")
- grid.raster(img)
-
